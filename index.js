023c1de6feb3df9a0f78f5d5cc0dfc0c8b8b88b1
Kaveh Saleminejad <lcaveh@gmail.com>
2018-08-09 23:36:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -39,8 +39,12 @@ function total() {
 }
 
 function removeFromCart(item) {
-  
-  
+  for (var i=0; i<cart.length; i++){
+    if (cart[i]['itemName'] = item) {
+      cart.splice(i,1);
+    }
+  }
+  return cart;
 }
 
 function placeOrder(cardNumber) {
